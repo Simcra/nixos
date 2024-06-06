f5710f145d6653208efd3377e7e73d3dee35b745
simcra <simcra@live.com>
2024-06-06 21:29:29 +0930

    Consolidate nix flakes back down

--- flake.nix
+++ flake.nix
@@ -4,7 +4,11 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
     automous-zones.url = "github:the-computer-club/automous-zones";
+
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs @ { 
@@ -12,13 +16,20 @@
     nixpkgs,
     nixpkgs-unstable,
     automous-zones,
+    home-manager,
     ...
   }: {
-    nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
     nixosConfigurations.voidhawk = nixpkgs.lib.nixosSystem {
       specialArgs = inputs;
-      modules = [ ./voidhawk.nix ];
+      modules = [
+        {
+          nix.settings.experimental-features = [ "nix-command" "flakes" ];
+          nixpkgs.config.allowUnfree = true;
+          system.stateVersion = "24.05";
+        }
+        ./wireguard.nix
+        ./voidhawk.nix
+      ];
     };
   };
 }
--- voidhawk.nix
+++ voidhawk.nix
@@ -1,7 +1,4 @@
 { config, lib, pkgs, ... } : {
-  nixpkgs.config.allowUnfree = true;
-  system.stateVersion = "24.05";
-
   # Hardware
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
@@ -20,13 +17,6 @@
   networking.useDHCP = lib.mkDefault true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
-  # Imported modules
-  imports = [
-    ./i18n-au-adl.nix
-    ./users.nix
-    ./wireguard.nix
-  ];
-
   # Networking
   networking.hostName = "voidhawk";
   networking.networkmanager.enable = true;
@@ -43,6 +33,25 @@
   {
     "172.16.2.1" = cypress;
   };
+
+  # Localization
+  time.timeZone = "Australia/Adelaide";
+
+  i18n.defaultLocale = "en_AU.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_AU.UTF-8";
+    LC_IDENTIFICATION = "en_AU.UTF-8";
+    LC_MEASUREMENT = "en_AU.UTF-8";
+    LC_MONETARY = "en_AU.UTF-8";
+    LC_NAME = "en_AU.UTF-8";
+    LC_NUMERIC = "en_AU.UTF-8";
+    LC_PAPER = "en_AU.UTF-8";
+    LC_TELEPHONE = "en_AU.UTF-8";
+    LC_TIME = "en_AU.UTF-8";
+  };
+
+  services.xserver.xkb.layout = "au";
+  services.xserver.xkb.variant = "";
 
   # X11 / Desktop Environment
   services.xserver.enable = true;
@@ -62,6 +71,14 @@
     pulse.enable = true;
   };
 
+  # Users
+  users.users.simcra = {
+    isNormalUser = true;
+    description = "simcra";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
   # Programs
   programs.firefox.enable = true;
   programs.steam = {
@@ -75,22 +92,22 @@
     git
     vim
     wget
-    (vscode-with-extensions.override {
-      vscodeExtensions = with vscode-extensions; [
-        ms-azuretools.vscode-docker
-        ms-python.python
-        ms-vscode.cpptools
-        ms-vscode-remote.remote-ssh
-        # Nix
-        bbenoist.nix
-        # Lua
-        sumneko.lua
-        # Rust
-        rust-lang.rust-analyzer
-        serayuzgur.crates
-        njpwerner.autodocstring
-      ];
-    })
+#    (vscode-with-extensions.override {
+#      vscodeExtensions = with vscode-extensions; [
+#        ms-azuretools.vscode-docker
+#        ms-python.python
+#        ms-vscode.cpptools
+#        ms-vscode-remote.remote-ssh
+#        # Nix
+#        bbenoist.nix
+#        # Lua
+#        sumneko.lua
+#        # Rust
+#        rust-lang.rust-analyzer
+#        serayuzgur.crates
+#        njpwerner.autodocstring
+#      ];
+#    })
   ];
 
   # VirtualBox
