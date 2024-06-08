d662b6d7d2bbde7d0403b7c24dc0c0c204dea55f
Simcra <simcra@live.com>
2024-06-08 21:40:01 +0930

    Add nordpass to firefox

--- users/simcra.nix
+++ users/simcra.nix
@@ -43,6 +43,19 @@
           ublock-origin
         ];
       };
+      policies = {
+        ExtensionSettings = let 
+          extension = shortId: uuid: {
+            name = uuid;
+            value = {
+              install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${shortId}/latest.xpi";
+              installation_mode = "normal_installed";
+            };
+          };
+          in builtins.listToAttrs [
+            (extension "nordpass-password-management" "nordpassStandalone@nordsecurity.com")
+          ];
+      };
     };
 
     # VSCode with extensions
