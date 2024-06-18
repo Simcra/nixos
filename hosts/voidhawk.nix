de9c5b3fc86ee1e3f2462d3af14264db2bc6acb2
Simcra <simcra@live.com>
2024-06-18 13:14:01 +0930

    Add MegaCLI and some optimizations for gaming to voidhawk

--- hosts/voidhawk.nix
+++ hosts/voidhawk.nix
@@ -1,10 +1,11 @@
-{ config, lib, ... }: {
+{ config, lib, pkgs, ... }: {
   # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.initrd.availableKernelModules = [ "vmd" "xhci_pci" "megaraid_sas" "ahci" "thunderbolt" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_xanmod_stable; # This makes KSP run like dogwater
   boot.extraModulePackages = [ ];
 
   # Filesystem
@@ -73,11 +74,22 @@
     pulse.enable = true;
   };
 
-  # Programs
-  programs.direnv.enable = true; # This guy can be put in home-manager, but it doesn't work if you do, DONT DO THAT
+  # System
+  environment.systemPackages = [
+    pkgs.megacli # Voidhawk has a MegaRAID card in it
+  ];
+  programs.direnv = {
+    enable = true; # For some reason direnv don't work properly when you put it in home-manager, so here it stays
+  };
+
+  # Steam
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+  programs.gamemode = {
+    enable = true;
+    enableRenice = true;
   };
 }
