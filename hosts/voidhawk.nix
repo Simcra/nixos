3f1e3c6e32f93f961cf45b0d549d8dee154c5a5f
Simcra <simcra@live.com>
2024-06-19 14:12:00 +0930

    Update flake.lock, clean up voidhawk.nix

--- hosts/voidhawk.nix
+++ hosts/voidhawk.nix
@@ -25,41 +25,53 @@
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
   # NVIDIA
-  hardware.opengl.enable = true;
-  hardware.opengl.driSupport = true;
-  hardware.opengl.driSupport32Bit = true;
-  hardware.nvidia.modesetting.enable = true;
-  hardware.nvidia.powerManagement.enable = false;
-  hardware.nvidia.powerManagement.finegrained = false;
-  hardware.nvidia.open = false;
-  hardware.nvidia.nvidiaSettings = true;
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement = {
+      enable = false;
+      finegrained = false;
+    };
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 
   # Networking
-  networking.hostName = "voidhawk";
-  networking.networkmanager.enable = true;
-  networking.useDHCP = lib.mkDefault true;
-  networking.wireguard.interfaces.asluni.ips = [ "172.16.2.12/32" ];
-  networking.hosts =
-    let
-      cypress = [
-        "cypress.local"
-        "sesh.cypress.local"
-        "tape.cypress.local"
-        "codex.cypress.local"
-        "chat.cypress.local"
-      ];
-    in
-    {
-      "172.16.2.1" = cypress;
-    };
+  networking = {
+    hostName = "voidhawk";
+    networkmanager.enable = true;
+    useDHCP = lib.mkDefault true;
+    wireguard.interfaces.asluni.ips = [ "172.16.2.12/32" ];
+    hosts =
+      let
+        cypress = [
+          "cypress.local"
+          "sesh.cypress.local"
+          "tape.cypress.local"
+          "codex.cypress.local"
+          "chat.cypress.local"
+        ];
+      in
+      {
+        "172.16.2.1" = cypress;
+      };
+  };
 
   # X11 / Desktop Environment
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.gdm.autoSuspend = false;
-  services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.videoDrivers = [ "nvidia" ];
+  services.xserver = {
+    enable = true;
+    displayManager.gdm = {
+      enable = true;
+      autoSuspend = false;
+    };
+    desktopManager.gnome.enable = true;
+    videoDrivers = [ "nvidia" ];
+  };
 
   # Printing
   services.printing.enable = true;
@@ -69,8 +81,10 @@
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
     pulse.enable = true;
   };
 
