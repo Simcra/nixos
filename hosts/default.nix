8a734902c74c051cec53f7b8c75e2169bf479e20
Simcra <5228381+Simcra@users.noreply.github.com>
2024-07-08 19:49:26 +0930

    Revert some changes

--- hosts/default.nix
+++ hosts/default.nix
@@ -66,11 +66,12 @@
   security.rtkit.enable = lib.mkDefault true;
   services.pipewire = {
     enable = lib.mkDefault true;
+    audio.enable = lib.mkDefault true;
+    pulse.enable = lib.mkDefault true;
     alsa = {
       enable = lib.mkDefault true;
       support32Bit = lib.mkDefault true;
     };
-    pulse.enable = lib.mkDefault true;
     jack.enable = lib.mkDefault true;
   };
 
