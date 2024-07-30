498f64949704b3bf07b261b1f7a443ee04d3988b
Simcra <5228381+Simcra@users.noreply.github.com>
2024-07-30 20:38:15 +0930

    Update flake lock file, move default linux packages back into common configuration, update all machines to use latest linux kernel and recommended nvidia drivers

--- nixos/configurations/default.nix
+++ nixos/configurations/default.nix
@@ -41,6 +41,9 @@ in
     extraSpecialArgs = { inherit overlays; };
   };
 
+  # Use latest linux kernel by default
+  boot.kernelPackages = mkDefault pkgs.linuxPackages_latest;
+
   # Set default state version
   system.stateVersion = mkDefault "24.05";
 
