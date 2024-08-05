91e770fc93aa89170473040a6aeca4fb421b569c
Simcra <5228381+Simcra@users.noreply.github.com>
2024-08-05 16:40:47 +0930

    Add missing aliases for video hardware

--- nixos/modules/hardware/video/default.nix
+++ nixos/modules/hardware/video/default.nix
@@ -12,5 +12,7 @@ in
     (mkAliasOptionModule [ "hardware" "graphics" "extraPackages" ] [ "hardware" "opengl" "extraPackages" ])
     (mkAliasOptionModule [ "hardware" "graphics" "extraPackages32" ] [ "hardware" "opengl" "extraPackages32" ])
     (mkAliasOptionModule [ "hardware" "graphics" "enable32Bit" ] [ "hardware" "opengl" "driSupport32Bit" ])
+    (mkAliasOptionModule [ "hardware" "graphics" "package" ] [ "hardware" "opengl" "package" ])
+    (mkAliasOptionModule [ "hardware" "graphics" "package32" ] [ "hardware" "opengl" "package32" ])
   ];
 }
