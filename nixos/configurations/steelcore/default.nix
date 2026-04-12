2998e96585b96066c19ce7165e1c5e3c6c82826c
Simcra <5228381+Simcra@users.noreply.github.com>
2026-04-12 17:21:41 +0930

    Update configuration for steelcore

--- nixos/configurations/steelcore/default.nix
+++ nixos/configurations/steelcore/default.nix
@@ -6,6 +6,10 @@
 }:
 let
   rootDir = ../../..;
+  nvidiaPackages = import (rootDir + "/nixos/derivations/hardware/video/nvidia/kernel-packages.nix") {
+    inherit config;
+    inherit pkgs;
+  };
   hostname = "steelcore";
   usernames = [
     "darkcrystal"
@@ -23,5 +27,113 @@ in
     username: import (rootDir + "/home-manager/configurations/${hostname}/${username}.nix")
   );
 
-  #TODO
+  # Boot configuration
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    kernelPackages = pkgs.linuxPackages_latest;
+    kernelModules = [ "kvm-intel" ];
+    initrd.availableKernelModules = [
+      "ahci"
+      "nvme"
+      "sd_mod"
+      "usbhid"
+      "usb_storage"
+      "xhci_pci"
+    ];
+  };
+
+  # Filesystems
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/f3ebccfa-9f4c-43f1-a7f3-5842bd274d78";
+      fsType = "ext4";
+    };
+    "/boot" = {
+      device = "/dev/disk/by-uuid/C19D-F445";
+      fsType = "vfat";
+      options = [
+        "fmask=0077"
+        "dmask=0077"
+      ];
+    };
+  };
+  swapDevices = [ ];
+
+  # Locale
+  time.timeZone = "Australia/Adelaide";
+  i18n = rec {
+    defaultLocale = "en_AU.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = defaultLocale;
+      LC_IDENTIFICATION = defaultLocale;
+      LC_MEASUREMENT = defaultLocale;
+      LC_MONETARY = defaultLocale;
+      LC_NAME = defaultLocale;
+      LC_NUMERIC = defaultLocale;
+      LC_PAPER = defaultLocale;
+      LC_TELEPHONE = defaultLocale;
+      LC_TIME = defaultLocale;
+    };
+  };
+  services.xserver.xkb = {
+    layout = "au";
+    variant = "";
+  };
+
+  # Hardware
+  hardware = {
+    cpu.intel.updateMicrocode = config.hardware.enableRedistributableFirmware;
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+    nvidia = {
+      modesetting.enable = true;
+      powerManagement = {
+        enable = false;
+        finegrained = false;
+      };
+      open = false;
+      nvidiaSettings = true;
+      nvidiaPersistenced = false;
+      package = nvidiaPackages.latest;
+    };
+  };
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  # Network
+  networking = {
+    firewall = {
+      # Spotify
+      allowedTCPPorts = [ 57621 ];
+      allowedUDPPorts = [ 5353 ];
+    };
+  };
+
+  # Programs
+  programs = {
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+      gamescopeSession.enable = true;
+    };
+    gamemode = {
+      enable = true;
+      enableRenice = true;
+    };
+  };
+
+  # Environment
+  environment = {
+    variables.VDPAU_DRIVER = "nvidia";
+    sessionVariables.LIBVA_DRIVER_NAME = "nvidia";
+    systemPackages = with pkgs; [
+      mangohud # FPS counter and performance overlay
+    ];
+  };
 }
